4954ec8dd2fa8c5a45ca26d5bf2b999569a815b7
Simi Oyin <141048058+simioyin222@users.noreply.github.com>
2023-11-17 10:29:45 -0600

    Initial commit

---
+++ goodfailtest/template.test.js
@@ -0,0 +1,8 @@
+// import { Function } from './../src/js/template.js';
+
+// describe('Template Function', () => {
+
+//   test('should do this behavior', () => {
+//     // expect(functionAnswer).toEqual(expectedAnswer);
+//   });
+// });
