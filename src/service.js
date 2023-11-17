458bb9ee43044b925b25b70d1abad77259914c56
Simi O <coolkid1984@ymail.com>
2023-11-17 13:03:47 -0600

    change api connection

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
-export default class CurrencyExchangeService {
-  static async getAvailableCurrencies() {
-    const response = await fetch("https://v6.exchangerate-api.com/v6/48e7a012fc5cb0ad3fe17772/latest/USD")
-    const data = await response.json(); 
+export default class ExchangeService {
+  static getConversionRate(baseAmount, baseCode, targetCode) {
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${targetCode}/${baseAmount}`;
+    return fetch(url)
   }
 }
