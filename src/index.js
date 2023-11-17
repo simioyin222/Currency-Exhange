0771b3245c1f52a9b60d0f490b5f2494aa83d3cb
Simi O <coolkid1984@ymail.com>
2023-11-17 12:28:34 -0600

    remove from index.js copied to service.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
-export default class CurrencyExchangeService {
-  static async getAvailableCurrencies() {
-    const response = await fetch("") 
-  }
-}
--- src/service.js
+++ src/service.js
@@ -1,1 +1,6 @@
-export 
+export default class CurrencyExchangeService {
+  static async getAvailableCurrencies() {
+    const response = await fetch("https://v6.exchangerate-api.com/v6/48e7a012fc5cb0ad3fe17772/latest/USD")
+    const data = await response.json(); 
+  }
+}
