65c3d31dc8377aa448ffbed5ad0e82f39ed21ef3
Simi O <coolkid1984@ymail.com>
2023-11-17 14:23:22 -0600

    writing export function of making caculations

--- js/exchange.js
+++ js/exchange.js
@@ -1,2 +1,9 @@
 import ExchangeService from './../services/exchange-service.js';
 import { displayConversion, displayError, createSelectionForms } from './../index.js';
+
+export function getConversionRate(baseAmount, baseCode, queryCode) {
+  ExchangeService.getConversionRate(baseAmount, baseCode, queryCode)
+  .then((conversion) => {
+    
+  }
+}
