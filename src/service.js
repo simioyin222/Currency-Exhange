0b50294bbe14a98ef66ba1cda01ab6ad583aab12
Simi O <coolkid1984@ymail.com>
2023-11-17 13:55:49 -0600

    base api call functional on service/js

--- src/service.js
+++ src/service.js
@@ -2,5 +2,22 @@ export default class ExchangeService {
   static getConversionRate(baseAmount, baseCode, targetCode) {
     const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCode}/${targetCode}/${baseAmount}`;
     return fetch(url)
+      .then(function (response) {
+        if (!response.ok) {
+          return response.json().then(function (apiResponse) {
+            let apiError = apiResponse["error-type"];
+            if (response.status === 404) {
+              apiError = "Currency not found.";
+            }
+            const errorMessage = `${response.status}: ${apiError}`;
+            throw new Error(errorMessage);
+          });
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
+  
 }
