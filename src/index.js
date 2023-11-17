1cdaf1901013bb97e1799466372ce83d6fd158c4
Simi O <coolkid1984@ymail.com>
2023-11-17 12:20:08 -0600

    add first async function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
- import 'bootstrap';
- import 'bootstrap/dist/css/bootstrap.min.css';
- import './css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+export default class CurrencyExchangeService {
+  static async getAvailableCurrencies() {
+    const response = await fetch("") 
+  }
+}
